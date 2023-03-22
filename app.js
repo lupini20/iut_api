aafb23d971215f805403caa6b931526cfc6ee81e
lupini20 <medilupini@gmail.com>
2023-03-22 20:29:02 +0100

    first commit yes

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var studentRouter = require('./routes/student');
+var mongoose = require('mongoose');
+
+var connectionString = "mongodb+srv://medilupini:Medi0816922122@medi.dib8b2m.mongodb.net/test";
+var mongoDB = process.env.MONGODB_URI || connectionString;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/students', studentRouter);
+
+
+
+module.exports = app;
